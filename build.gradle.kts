c9cc5ac1c636e1192c2345205204e632e8d5dbcb
lijun <lijunyxm@qq.com>
2022-09-26 18:22:11 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "com.yxmcute"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
---
+++ src/main/java/com/yxmcute/Main.kt
@@ -0,0 +1,23 @@
+package com.yxmcute
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello world!")
+    }
+}
+suspend fun performRequest(request: Int): String {
+    delay(1000) // imitate long-running asynchronous work
+    return "response $request"
+}
+
+fun main() = runBlocking<Unit> {
+    (1..3).asFlow() // a flow of requests
+        .transform { request ->
+            emit("Making request $request")
+            emit(performRequest(request))
+        }
+        .collect { response -> println(response) }
+}

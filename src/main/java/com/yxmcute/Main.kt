2ad70b2155692749b69250af11ccfe888f278474
lijun <lijunyxm@qq.com>
2022-09-26 18:59:45 +0800

    add method

--- src/main/java/com/yxmcute/Main.kt
+++ src/main/java/com/yxmcute/Main.kt
@@ -1,4 +1,5 @@
 package com.yxmcute
+
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
@@ -8,12 +9,21 @@ object Main {
         println("Hello world!")
     }
 }
+
 suspend fun performRequest(request: Int): String {
     delay(1000) // imitate long-running asynchronous work
     return "response $request"
 }
 
 fun main() = runBlocking<Unit> {
+
+    // mainTake()
+   // mainTransform()
+    val sum= reduce()
+    println(sum)
+}
+
+suspend fun mainTransform() {
     (1..3).asFlow() // a flow of requests
         .transform { request ->
             emit("Making request $request")
@@ -21,3 +31,26 @@ fun main() = runBlocking<Unit> {
         }
         .collect { response -> println(response) }
 }
+
+//take限长操作符
+fun numbers(): Flow<Int> = flow {
+    try {
+        emit(1)
+        emit(2)
+        println("This line will not execute")
+        emit(3)
+    } finally {
+        println("Finally in numbers")
+    }
+}
+
+fun mainTake() = runBlocking<Unit> {
+    numbers().take(2).collect { value ->
+        println(value)
+    }
+}
+ suspend  fun reduce()=(1..5).asFlow()
+    .map { it * it } // 数字 1 至 5 的平方
+    .reduce { a, b -> a + b } // 求和（末端操作符）
+
+
